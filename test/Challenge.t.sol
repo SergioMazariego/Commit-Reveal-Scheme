dd378553e68751ee0e5448628aab531c66f05d26
Your Name <sergio.mazariego@hotmail.com>
2023-06-18 21:11:37 -0600

    testRevealWinnerMultipleTimes test added

--- test/Challenge.t.sol
+++ test/Challenge.t.sol
@@ -48,10 +48,10 @@ contract ChallengeTest is Test {
     }
 
     function testRevealWinnerWithoutStartingPhase() public {
-        // Attempt to reveal the winner using the revealWinner function before starting the reveal phase.
-        vm.expectRevert("Reveal phase has not started yet");
-        challenge.reveal("test");
         // Verify that the function reverts with the appropriate error message.
+        vm.expectRevert("Reveal phase has not started yet");
+        // Attempt to reveal the winner using the revealWinner function before starting the reveal phase.
+        challenge.reveal("test");
     }
 
     function testRevealWinnerWithoutCommits() public {
@@ -64,25 +64,45 @@ contract ChallengeTest is Test {
     }
 
     function testRevealWinnerIncorrectSecretHash() public {
+        // Commit an incorrect answer
+        vm.prank(address(1));
+        challenge.commit(keccak256(abi.encodePacked("incorrect answer")));
         // Start the reveal phase using the startRevealPhase function.
-
+        challenge.startRevealPhase();
         // Reveal an answer using the reveal function.
-
-        // Attempt to reveal the winner using the revealWinner function with an incorrect secret hash.
-
+        vm.prank(address(1));
+        challenge.reveal("incorrect answer");
         //  Verify that the function reverts with the appropriate error message.
+        vm.expectRevert("No winner found");
+        // Attempt to reveal the winner using the revealWinner function with an correct secret hash.
+        challenge.revealWinner(keccak256(abi.encodePacked("correct answer")));
     }
 
     function testRevealWinnerMultipleTimes() public {
+        // Commit some answers
+        vm.prank(address(1));
+        challenge.commit(keccak256(abi.encodePacked("answer 1")));
+        vm.prank(address(2));
+        challenge.commit(keccak256(abi.encodePacked("answer 2")));
         //  Start the reveal phase using the startRevealPhase function.
-
+        challenge.startRevealPhase();
         //  Reveal an answer using the reveal function.
-
+        vm.prank(address(1));
+        vm.expectEmit();
+        emit AnswerRevealed(address(1), "answer 1");
+        challenge.reveal("answer 1");
+        vm.prank(address(2));
+        vm.expectEmit();
+        emit AnswerRevealed(address(2), "answer 2");
+        challenge.reveal("answer 2");
         //  Reveal the winner using the revealWinner function.
-
-        //  Attempt to reveal the winner again.
-
+        vm.expectEmit();
+        emit WinnerRevealed(address(1), "answer 1");
+        challenge.revealWinner(keccak256(abi.encodePacked("answer 1")));
         //  Verify that the function reverts with the appropriate error message.
+        vm.expectRevert("Winner already revealed");
+        //  Attempt to reveal the winner again.
+        challenge.revealWinner(keccak256(abi.encodePacked("again")));
     }
 
     function testCommitAfterRevealPhaseStarted() public {
